fa00fe4208e7f73304f876b1620ee3bad3863580
李焘 <belleingsoft@gmail.com>
2015-07-21 17:46:44 +0800

    Create ops-tools.sh

---
+++ ops-tools.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function addRpmforgeResource(){
+        wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+        sudo rpm -ivh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+        rm -f rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+}
+
+function installTools(){
+        sudo yum install htop
+}
+
+echo "please select your choose"
+echo "which do you want to?input the number."
+echo "1. install htop"
+echo "2. install htop"
+read input
+
+case "$input" in
+[1])
+        addRpmforgeResource
+        installTools
+;;
+[2])
+        addRpmforgeResource
+        installTools
+;;
+*) echo "nothing,exit";;
+esac
